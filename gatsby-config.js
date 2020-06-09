2f70d47c15b42d7713d7774975da1c8f569d59f4
Daniel Ward <dward1502@gmail.com>
2020-06-09 11:47:33 -0700

    revamp project to work with netlify and netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,80 +1,77 @@
 module.exports = {
   siteMetadata: {
-    title: `Ancient History`,
-    description: `Dive into the ancient history both accepted and theoretical beliefs of what was. In depth discussion on the younger dryas impact hypothesis, solar flares and the effect it had on pre deluvian civilizations.`,
-    author: `@gatsbyjs`,
+    title: 'Gatsby + Netlify CMS Starter',
+    description:
+      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
+      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
-        name: `uploads`,
+        name: 'uploads',
       },
     },
     {
-      resolve:'gatsby-source-filesystem',
-      options:{
-        path:`${__dirname}/src/pages`,
-        name:'pages',
-      }
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
     },
     {
-      resolve:'gatsby-source-filesystem',
-      options:{
-        path:`${__dirname}/src/assets/images`,
-        name:'images'
-      }
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/img`,
+        name: 'images',
+      },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve:'gatsby-transformer-remark',
-      options:{
-        plugins:[
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
           {
-            resolve:'gatsby-remark-relative-images',
-            options:{
-              name:'uploads'
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
             },
           },
           {
-            resolve:'gatsby-remark-images',
-            options:{
-              maxWidth:2048
-            }
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
           },
           {
-            resolve:'gatsby-remark-copy-linked-files',
-            options:{
-              destinationDir:'static'
-            }
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
           },
-        ]
-      }
-    },
-    {
-      resolve:'gatsby-plugin-netlify-cms',
-      options:{
-        modulePath:`${__dirname}/src/cms/cms.js`
-      }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `ancient-history`,
-        short_name: `AH`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/AncientHistoryLogo.png`, // This path is relative to the root of the site.
+        ],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      options: {
+        develop: true, // Activates purging in npm run develop
+        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+      },
+    }, // must be after other CSS plugins
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,43 +1,58 @@
-import React, { useState} from "react"
-import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import React from 'react'
+import { Helmet } from 'react-helmet'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import './all.sass'
+import useSiteMetadata from './SiteMetadata'
+import { withPrefix } from 'gatsby'
 
-import "../assets/stylesheet/index.scss"
-
-import Navbar from "../components/Navbar/Navbar"
-import SideBar from "../components/Navbar/Sidebar"
-
-
-const Layout = ({ children }) => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-  const [showSideBar, setShowSideBar] = useState(false)
-
-  const sideBarClosedHandler = () => {
-    setShowSideBar(false)
-  }
-  const sideBarToggleHandler = () => {
-    setShowSideBar(true)
-  }
+const TemplateWrapper = ({ children }) => {
+  const { title, description } = useSiteMetadata()
   return (
-    <>
-      <Navbar SideBarToggleClicked={sideBarToggleHandler} />
-      <SideBar open={showSideBar} closed={sideBarClosedHandler} />
-      
-        <main>{children}</main>
-    </>
+    <div>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href={`${withPrefix('/')}img/apple-touch-icon.png`}
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-32x32.png`}
+          sizes="32x32"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href={`${withPrefix('/')}img/favicon-16x16.png`}
+          sizes="16x16"
+        />
+
+        <link
+          rel="mask-icon"
+          href={`${withPrefix('/')}img/safari-pinned-tab.svg`}
+          color="#ff4400"
+        />
+        <meta name="theme-color" content="#fff" />
+
+        <meta property="og:type" content="business.business" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content="/" />
+        <meta
+          property="og:image"
+          content={`${withPrefix('/')}img/og-image.jpg`}
+        />
+      </Helmet>
+      <Navbar />
+      <div>{children}</div>
+      <Footer />
+    </div>
   )
 }
 
-// Layout.propTypes = {
-//   children: PropTypes.node.isRequired,
-// }
-
-export default Layout
+export default TemplateWrapper
